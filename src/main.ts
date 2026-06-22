1e393b9b84aaf8a9590b4b8e69a0394286492e96
Ibrohim Meliqulov <ibrohim.meliqulov2003@gmail.com>
2026-06-22 12:15:29 +0500

    fix: listen on 0.0.0.0 with PORT env

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-  })
+  });
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); // '0.0.0.0' muhim!
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
