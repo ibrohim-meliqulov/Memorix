22591fad54160cecc3a12ee18d0ba0629833c6cf
Ibrohim Meliqulov <ibrohim.meliqulov2003@gmail.com>
2026-06-22 11:59:39 +0500

    fix: enable CORS for all origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
